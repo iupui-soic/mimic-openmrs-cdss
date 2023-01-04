2ef7e3faad66c59c589e79796c79fd2bb906ffcf
Saptarshi Purkayastha <sunbiz@gmail.com>
2023-01-03 23:53:32 -0500

    adding module to show cdss in patient tab

---
+++ openmrs-module-cdss/omod/src/main/java/org/openmrs/module/cdss/extension/html/CdssTab.java
@@ -0,0 +1,53 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.cdss.extension.html;
+
+import org.openmrs.module.Extension;
+import org.openmrs.module.web.extension.PatientDashboardTabExt;
+
+/**
+ * This class defines the tab that will appear on the patient dashboard with the
+ * "cdss.title" tab name.
+ */
+public class CdssTab extends PatientDashboardTabExt {
+	
+	/**
+	 * @see PatientDashboardTabExt#getMediaType()
+	 */
+	@Override
+	public Extension.MEDIA_TYPE getMediaType() {
+		return Extension.MEDIA_TYPE.html;
+	}
+
+	@Override
+	public String getTabName() {
+		return "cdss.title";
+	}
+
+	@Override
+	public String getTabId() {
+		return "cdss";
+	}
+
+	@Override
+	public String getRequiredPrivilege() {
+		return null;
+	}
+
+	@Override
+	public String getPortletUrl() {
+		return null;
+	}
+	
+}
---
+++ openmrs-module-cdss/omod/src/main/webapp/cdss.jsp
@@ -0,0 +1,7 @@
+<%@ include file="/WEB-INF/template/include.jsp"%>
+<%@ include file="/WEB-INF/template/header.jsp"%>
+
+<h2><spring:message code="cdss.title"/></h2>
+<br/>
+
+<%@ include file="/WEB-INF/template/footer.jsp"%>
